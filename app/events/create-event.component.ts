23762be78203ca57b09156adff1ef8c19420bf38
ruparee <eruparee@me.com>
2017-07-04 22:16:43 +0100

    Diving deeper into template-based forms

--- app/events/create-event.component.ts
+++ app/events/create-event.component.ts
@@ -4,6 +4,7 @@
 
 import { Component } from '@angular/core'
 import { Router } from '@angular/router'
+import { EventService } from "./shared/index";
 
 @Component ({
     templateUrl: 'app/events/create-event.component.html',
@@ -18,12 +19,15 @@ import { Router } from '@angular/router'
 })
 export class CreateEventComponent {
     isDirty:boolean = true
-    constructor(private router: Router) {
+    constructor(private router: Router, private eventService:EventService) {
 
     }
 
     saveEvent(formValues) {
-        console.log(formValues)
+        // console.log(formValues)
+        this.eventService.saveEvent(formValues)
+        this.isDirty = false
+        this.router.navigate(['/events'])
     }
 
     cancel() {
