984bb718a4c0b636e43cb0ac949f84958b2be212
ruparee <eruparee@me.com>
2017-07-04 19:34:39 +0100

    Added reactive forms for user profile

--- app/user/user.module.ts
+++ app/user/user.module.ts
@@ -4,7 +4,7 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterModule } from '@angular/router'
-import { FormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 import { userRoutes } from './user.routes'
 import { ProfileComponent } from './profile.component'
@@ -14,6 +14,7 @@ import { LoginComponent } from './login.component'
     imports: [
         CommonModule,
         FormsModule,
+        ReactiveFormsModule,
         RouterModule.forChild(userRoutes)
     ],
     declarations: [
