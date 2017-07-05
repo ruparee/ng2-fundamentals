476307bcbb889aaa9c6420275d294f6599474858
ruparee <eruparee@me.com>
2017-07-06 00:28:24 +0100

    Angular dependency injection lookup

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core'
-import { EventService } from './shared/event.service'
-import { ToastrService } from '../common/toastr.service'
-import { ActivatedRoute } from '@angular/router'
-import { IEvent } from './shared/index'
+import {Component, OnInit} from '@angular/core'
+import {EventService} from './shared/event.service'
+import {ActivatedRoute} from '@angular/router'
+import {IEvent} from './shared/index'
 
 @Component({
     // selector: 'event-list',
@@ -12,28 +11,23 @@ import { IEvent } from './shared/index'
         <hr />
         <div class="row">
             <div *ngFor="let event of events" class="col-md-5">
-                <event-thumbnail (click) ="handleThumbnailClick(event.name)"
-                [event]="event"></event-thumbnail>
+                <event-thumbnail [event]="event"></event-thumbnail>
             </div>
         </div>
      </div>
     `
 })
-export class EventsListComponent  implements OnInit {
+export class EventsListComponent implements OnInit {
     // events:any[]
     // events:any
     events:IEvent[]
 
-    constructor(private eventService: EventService, private toastrService: ToastrService,
-    private route:ActivatedRoute ) {
- 
+    constructor(private eventService:EventService,
+                private route:ActivatedRoute) {
+
     }
 
     ngOnInit() {
         this.events = this.route.snapshot.data['events']
-    }
-
-    handleThumbnailClick(eventName) {
-        this.toastrService.success(eventName)
     }
 }
