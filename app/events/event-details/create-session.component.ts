f27ded57d50276ce89f6a1c9ad2d6ccd9ecf3480
ruparee <eruparee@me.com>
2017-07-05 13:02:42 +0100

    First Revision

--- app/events/event-details/create-session.component.ts
+++ app/events/event-details/create-session.component.ts
@@ -1,11 +1,12 @@
 /**
  * Created by erupare on 04/07/2017.
  */
-import { Component, OnInit } from '@angular/core'
-import { FormControl, FormGroup, Validators} from '@angular/forms'
-import { ISession, restrictedWords } from '../shared/index'
+import {Component, OnInit, Output, EventEmitter} from '@angular/core'
+import {FormControl, FormGroup, Validators} from '@angular/forms'
+import {ISession, restrictedWords} from '../shared/index'
 
 @Component({
+    selector: 'create-session',
     templateUrl: 'app/events/event-details/create-session.component.html',
     styles: [`
         em { float:right; color: #E05C65; padding-left: 10px; }
@@ -18,12 +19,17 @@ import { ISession, restrictedWords } from '../shared/index'
 })
 
 export class CreateSessionComponent implements OnInit {
-    newSessionForm: FormGroup
-    name: FormControl
-    presenter: FormControl
-    duration: FormControl
-    level: FormControl
-    abstract: FormControl
+    @Output()
+    saveNewSession = new EventEmitter()
+    @Output()
+    cancelAddSession = new EventEmitter()
+
+    newSessionForm:FormGroup
+    name:FormControl
+    presenter:FormControl
+    duration:FormControl
+    level:FormControl
+    abstract:FormControl
 
     ngOnInit() {
         this.name = new FormControl('', Validators.required)
@@ -49,11 +55,16 @@ export class CreateSessionComponent implements OnInit {
             name: formValues.name,
             duration: +formValues.duration,
             level: formValues.level,
-            presenter: formValues,
+            presenter: formValues.presenter,
             abstract: formValues.abstract,
             voters: []
         }
-        console.log(session)
+        // console.log(session)
+        this.saveNewSession.emit(session)
+    }
+
+    cancel() {
+        this.cancelAddSession.emit()
     }
 
 }
