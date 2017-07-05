6067b3ed950f016dc46fe871c05d94a4fe9dfd62
ruparee <eruparee@me.com>
2017-07-05 21:10:14 +0100

    Filtering data

--- app/events/event-details/session-list.component.ts
+++ app/events/event-details/session-list.component.ts
@@ -2,15 +2,40 @@
  * Created by erupare on 05/07/2017.
  */
 
-import { Component, Input } from '@angular/core'
+import {Component, Input, OnChanges} from '@angular/core'
 import { ISession } from "../shared/index";
 
 
 @Component({
     selector: 'session-list',
-    templateUrl: 'app/events/event-details/session-list.component.html'
+    templateUrl: 'app/events/event-details/session-list.component.html',
+    styles: ['collapsible-well h6 {margin-top:-5px; margin-bottom:10px }'],
 })
 
-export class SessionListComponent {
-    @Input() sessions:ISession[]
+export class SessionListComponent implements OnChanges {
+    @Input()
+    sessions:ISession[];
+    @Input()
+    filterBy:string;
+    visibleSessions:ISession[] = [];
+
+    ngOnChanges() {
+        if (this.sessions) {
+            this.filterSessions(this.filterBy);
+        }
+    }
+
+    filterSessions(filter) {
+        console.log("filter : " + filter);
+
+        if (filter === 'all') {
+
+            // console.log("this.sessions.slice(0) : " + this.sessions.slice(0))
+            this.visibleSessions = this.sessions.slice(0);
+        } else {
+            this.visibleSessions = this.sessions.filter(session => {
+                return session.level.toLocaleLowerCase() === filter;
+            })
+        }
+    }
 }
