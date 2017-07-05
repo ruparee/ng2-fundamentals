ca8f6a5ed885d1b23c3ec26c09895bd1b118d01d
ruparee <eruparee@me.com>
2017-07-05 16:37:18 +0100

    Using built-in pipes

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,10 @@
 /**
  * Created by erupare on 18/06/2017.
  */
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
-import { RouterModule }  from '@angular/router'
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import {NgModule, LOCALE_ID} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
+import {RouterModule}  from '@angular/router'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 
 import {
     EventsListComponent,
@@ -17,29 +17,13 @@ import {
     CreateSessionComponent,
     SessionListComponent
 } from './events/index'
-import { EventsAppComponent } from './events-app.component'
-import { NavBarComponent } from './nav/navbar.component'
-import { ToastrService }  from './common/toastr.service'
+import {EventsAppComponent} from './events-app.component'
+import {NavBarComponent} from './nav/navbar.component'
+import {ToastrService}  from './common/toastr.service'
 import {CollapsibleWellComponent} from './common/collapsible-well.component'
-import { appRoutes } from './routes'
-import { Error404Component } from './errors/404.component'
-import { AuthService } from './user/auth.service'
-
-
-// import { EventsAppComponent } from './events-app.component'
-// import { EventsListComponent } from './events/events-list.component'
-// import { EventThumbnailComponent } from
-//     './events/event-thumbnail.component'
-// import  { NavBarComponent } from './nav/navbar.component'
-// import { EventService } from './events/shared/event.service'
-// import { ToastrService }  from './common/toastr.service'
-// import { EventDetailsComponent } from './events/event-details/event-details.component'
-// import { appRoutes } from './routes'
-// import { CreateEventComponent } from './events/create-event.component'
-// import { Error404Component } from './errors/404.component'
-// import { EventRouteActivator } from './events/event-details/event-route-activator.service'
-// import { EventListResolver } from './events/events-list-resolver.service'
-
+import {appRoutes} from './routes'
+import {Error404Component} from './errors/404.component'
+import {AuthService} from './user/auth.service'
 
 
 @NgModule({
@@ -49,14 +33,14 @@ import { AuthService } from './user/auth.service'
         ReactiveFormsModule,
         RouterModule.forRoot(appRoutes)
     ],
-    declarations: [EventsAppComponent, 
-                   EventsListComponent,
-                   EventThumbnailComponent,
-                   EventDetailsComponent,
-                   NavBarComponent,
-                   CreateEventComponent,
-                   Error404Component,
-                   CreateSessionComponent,
+    declarations: [EventsAppComponent,
+        EventsListComponent,
+        EventThumbnailComponent,
+        EventDetailsComponent,
+        NavBarComponent,
+        CreateEventComponent,
+        Error404Component,
+        CreateSessionComponent,
         SessionListComponent,
         CollapsibleWellComponent
     ],
@@ -66,6 +50,7 @@ import { AuthService } from './user/auth.service'
         EventRouteActivator,
         EventListResolver,
         AuthService,
+        {provide: LOCALE_ID, useValue: "en-GB"}, //replace "en-US" with your locale
         { // We use this long method for Functions and shorthand above for Services
             provide: 'canDeactivateCreateEvent',
             useValue: checkDirtyState
@@ -73,11 +58,11 @@ import { AuthService } from './user/auth.service'
     ],
     bootstrap: [EventsAppComponent]
 })
-export class AppModule{
+export class AppModule {
 
 }
 
-function checkDirtyState (component:CreateEventComponent) {
+function checkDirtyState(component:CreateEventComponent) {
     if (component.isDirty)
         return window.confirm('You have not saved this event, do you really want to cancel?')
     return true
--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -1,37 +1,22 @@
-import { Component, Input } from '@angular/core'
-import { IEvent } from './shared/index'
+import {Component, Input} from '@angular/core'
+import {IEvent} from './shared/index'
 
 @Component({
     selector: 'event-thumbnail',
     template: `
     <div [routerLink]="['/events', event.id]" class="well hoverwell thumbnail">
-        <h2>{{event?.name}}</h2>
+        <h2>{{event?.name | uppercase}}</h2>
         <div>Date: {{event?.date}}</div>
-        <!-- <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
-            Time: {{event?.time}}
-            <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
-            <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
-            <span *ngSwitchDefault>(Normal Start)</span>
-        </div> -->
-        <!-- <div [style.color]="event?.time === '8:00 am' ? '#003300' : '#bbb'" [ngSwitch]="event?.time">
-            Time: {{event?.time}}
-            <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
-            <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
-            <span *ngSwitchDefault>(Normal Start)</span>
-        </div> -->
-         <!-- <div [ngStyle]="{'color': event?.time === '8:00 am' ? '#003300' : '#bbb', 'font-weight': event?.time === '8:00 am' ? 'bold' : 'normal'}" [ngSwitch]="event?.time">
-            Time: {{event?.time}}
-            <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
-            <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
-            <span *ngSwitchDefault>(Normal Start)</span>
-        </div> -->
+      
         <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">
             Time: {{event?.time}}
             <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
             <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
             <span *ngSwitchDefault>(Normal Start)</span>
         </div>
-        <div>Price: \${{event?.price}}</div>
+        <!--<div>Price: {{event?.price | currency:'EUR':true }}</div>-->
+        <!--<div>Price: {{event?.price | currency:'GBP':true }}</div>-->
+         <div>Price: {{event?.price | currency:'USD':true }}</div>
         <div *ngIf="event?.location">
             <span>Location: {{event?.location?.address}}</span>
             <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
@@ -51,33 +36,15 @@ import { IEvent } from './shared/index'
 })
 
 export class EventThumbnailComponent {
-   // @Input() event: any
-    @Input() event:IEvent
+    // @Input() event: any
+    @Input()
+    event:IEvent
 
-// Here we return an object ....
-//    getStartTimeClass(){
-//        const isEarlyStart = this.event && this.event.time === '8:00 am'
-//        return {green: isEarlyStart, bold: isEarlyStart}
-//    }
 
-// Here we return a String
-//  getStartTimeClass(){
-//        if(this.event && this.event.time === '8:00 am')
-//              return 'green bold'
-//        return ' '
-//    }
-
-// Here we return an Array
-//  getStartTimeClass(){
-//        if(this.event && this.event.time === '8:00 am')
-//              return ['green' , 'bold']
-//        return []  // return an empty array
-//  }
-
-     getStartTimeStyle():any{ // We use 'any' to satisfy TypeScript because the 2 objects we are returning are of different shapes
-       if(this.event && this.event.time === '8:00 am')
-             return {color: '#003300' , 'font-weight': 'bold'}
-       return {}  
-}
+    getStartTimeStyle():any { // We use 'any' to satisfy TypeScript because the 2 objects we are returning are of different shapes
+        if (this.event && this.event.time === '8:00 am')
+            return {color: '#003300', 'font-weight': 'bold'}
+        return {}
+    }
 
 }
