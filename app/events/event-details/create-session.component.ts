2d9d482e5f31a3fce1b3b2990915b4f513939195
ruparee <eruparee@me.com>
2017-07-05 01:51:13 +0100

    Creating custom validator version 1

--- app/events/event-details/create-session.component.ts
+++ app/events/event-details/create-session.component.ts
@@ -31,7 +31,7 @@ export class CreateSessionComponent implements OnInit {
         this.duration = new FormControl('', Validators.required)
         this.level = new FormControl('', Validators.required)
         this.abstract = new FormControl('', [Validators.required,
-            Validators.maxLength(400)])
+            Validators.maxLength(400), this.restrictedWords])
 
         this.newSessionForm = new FormGroup({
             name: this.name,
@@ -40,6 +40,13 @@ export class CreateSessionComponent implements OnInit {
             level: this.level,
             abstract: this.abstract
         })
+    }
+
+    private restrictedWords(control: FormControl):{[key: string]: any}
+    {
+        return control.value.includes('foo')
+            ? {'restrictedWords': 'foo'}
+            : null
     }
 
     saveSession(formValues) {
