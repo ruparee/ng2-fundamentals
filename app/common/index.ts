f8564e32b3aab3d8e3762eab5cfb935ee1732300
ruparee <eruparee@me.com>
2017-07-06 02:53:46 +0100

    Adding jQuery

---
+++ app/common/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by erupare on 06/07/2017.
+ */
+export * from './jQuery.service'
+export * from './toastr.service'
+export * from './collapsible-well.component'
---
+++ app/common/jQuery.service.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by erupare on 06/07/2017.
+ */
+
+import  {OpaqueToken, InjectionToken} from '@angular/core'
+
+/**
+ OpaqueToken Deprecation Notes
+ since v4.0.0 because it does not support type information, use InjectionToken<?> instead.
+ https://angular.io/api/core/OpaqueToken
+ **/
+
+// export let JQ_TOKEN = new OpaqueToken('jQueryß');
+
+export let JQ_TOKEN = new InjectionToken<String>('jQuery');
+
