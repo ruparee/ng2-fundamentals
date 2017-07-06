a853bfbf3fae45aaea0176a76f2f490745b00663
ruparee <eruparee@me.com>
2017-07-06 02:20:31 +0100

    Implementing the session search

--- app/common/toastr.service.ts
+++ app/common/toastr.service.ts
@@ -8,14 +8,11 @@ import  {OpaqueToken, InjectionToken} from '@angular/core'
  since v4.0.0 because it does not support type information, use InjectionToken<?> instead.
  https://angular.io/api/core/OpaqueToken
  **/
-<<<<<<< HEAD
-// export let TOASTR_TOKEN = new InjectionToken<String>('toastr');
-export let TOASTR_TOKEN = new OpaqueToken('toastr');
-=======
+
 // export let TOASTR_TOKEN = new OpaqueToken('toastr');
 
 export let TOASTR_TOKEN = new InjectionToken<String>('toastr');
->>>>>>> using-the-inject-decorator
+
 
 export interface Toastr {
     success (msg:string, title?:string):void;
